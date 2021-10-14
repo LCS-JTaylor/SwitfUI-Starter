78c7c8417e55efc9b773401a999f9ca5aec2f9b3
LCS-JTaylor <jtaylor@lcs.on.ca>
2021-10-14 08:52:27 -0400

    Initial Commit

---
+++ SwitfUI Starter/SwitfUI_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitfUI_StarterApp.swift
+//  SwitfUI Starter
+//
+//  Created by Jackson Taylor on 2021-10-14.
+//
+
+import SwiftUI
+
+@main
+struct SwitfUI_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
